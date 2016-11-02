e17e7627500ad9be8e71db6028f9e54df331dbb3
Bradmandu <bwfowler@g.coastal.edu>
2016-11-02 18:39:19 -0400

    Add files via upload

---
+++ RequestBox.java
@@ -0,0 +1,57 @@
+public class RequestBox{
+	
+	boolean accepted;
+
+	
+	//Location l;
+	//String name; 
+	//DisplayBox b;
+	
+	
+	/*public RequestBox(Profile prof, RRequest req ){
+		
+		setLoc(req);
+		setTime(req);
+		setName(prof);
+		
+		
+	} 
+	
+	
+	private void setLoc(RRequest req){
+		
+		location= req.getLoc();
+		
+	}
+	
+	private void setTime(RRequest req){
+		
+		time= req.getTime();
+		
+	}
+	
+	private void setName(Profile prof){
+		
+		name= prof.getName();
+		
+	}
+	
+	
+	public void setBox(){
+		
+		
+		set message box; probably through javascript
+	}
+	
+	*/
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+}
