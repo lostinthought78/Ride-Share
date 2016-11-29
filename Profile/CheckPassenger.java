6b96fd9d55a247dead3131f8aa99da35bb81aa82
lpan2016 <lpan@g.coastal.edu>
2016-11-29 17:18:30 -0500

    Update and rename Check.java to CheckPassenger.java

--- Profile/CheckPassenger.java
+++ Profile/CheckPassenger.java
@@ -4,8 +4,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
  
-public class Check {
- //µÇÂ½¼ì²é
+public class CheckPassenger {
+ //check whether the user has log in
         public static void checkPassenger(Passenger p)
         {
             try {
@@ -49,46 +49,5 @@ public class Check {
              
         }
         
-        public static void checkDriver(Driver d)
-        {
-            try {
-                Class.forName(" ");
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-            String url = " ";
-            String user = " ";
-            String password = " ";
-            Connection conn = null;
-            ResultSet rs = null;
-            PreparedStatement pst = null;
-             
-            String sql = "Select name from passenger where uName = ? and password = ? ";
-            try {
-                conn = DriverManager.getConnection(url, user, password);
-                pst = conn.prepareStatement(sql);
-                pst.setString(1, d.getuName());
-                pst.setString(2,d.getpassword());
-                rs = pst.executeQuery(); 
-                while(rs.next()){
-                    d.setuName(rs.getString(1));
-                }               
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }finally{
-                try {
-                    rs.close();
-                    pst.close();
-                    conn.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if(d.getuName() == null || d.getuName().isEmpty()){
-                System.out.println("Fail to login");
-            }else{
-                System.out.println("Welcome"+d.getuName()+"log in");
-            }
-             
-        }
+       
 }
